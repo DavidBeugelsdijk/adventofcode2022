f82e1b43b6a0ef8c1a1e7982dd78f4478defbc4a
DavidBeugelsdijk <112309@emmauscollege.nl>
2022-12-09 09:39:45 +0000

    day1 part1

--- day-1/script.py
+++ day-1/script.py
@@ -1,7 +1,32 @@
 inputFile = open('input.txt', 'r')
 lines = inputFile.readlines()
 
+newLines = []
+
 for line in lines:
-    line = line.strip()
+    newLines.append(line.replace("\n", ""))
+
+lines = newLines
+newLines = []
 
 print(lines)
+
+totCalElfs = [0]
+i = 0
+
+for line in lines:
+    if line.isdigit() :
+        totCalElfs[i] = totCalElfs[i] + int(line)
+    else:
+        totCalElfs.append(0)
+        i = i + 1
+
+print(totCalElfs)
+
+maxCal = 0
+
+for calories in totCalElfs:
+    if calories > maxCal:
+        maxCal = calories
+
+print(maxCal)
